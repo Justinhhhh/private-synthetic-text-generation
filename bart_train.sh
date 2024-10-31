86bbf243a19234f368ab134fcc90a58ea8234a32
SebOchs <48994875+SebOchs@users.noreply.github.com>
2024-10-31 11:59:07 +0100

    Update bart_train.sh

--- bart_train.sh
+++ bart_train.sh
@@ -1,26 +1,16 @@
-#!/bin/bash
-#
-#SBATCH --job-name=bart-e8
-#SBATCH --output=/ukp-storage-1/ochs/bart-e8.txt
-#SBATCH --account=ukp-researcher
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=1
-#SBATCH --mem=64GB
-#SBATCH --gres=gpu:a100:1
-
 cd baselines
 
 # np
-# python training.py --dataset phishing --epochs 4 --batch_size 32 --model bart
-# python training.py --dataset swmh --epochs 4 --batch_size 32 --model bart
-# python training.py --dataset thumbs-up --epochs 4 --batch_size 32 --model bart
-# python training.py --dataset webmd --epochs 4 --batch_size 32 --model bart
+python training.py --dataset phishing --epochs 4 --batch_size 32 --model bart
+python training.py --dataset swmh --epochs 4 --batch_size 32 --model bart
+python training.py --dataset thumbs-up --epochs 4 --batch_size 32 --model bart
+python training.py --dataset webmd --epochs 4 --batch_size 32 --model bart
 
 # e3
-# python training.py --dataset phishing --epochs 8 --batch_size 32 --private True --epsilon 3 --model bart
-# python training.py --dataset swmh --epochs 8 --batch_size 32 --private True --epsilon 3 --model bart
-# python training.py --dataset thumbs-up --epochs 8 --batch_size 32 --private True --epsilon 3 --model bart
-# python training.py --dataset webmd --epochs 8 --batch_size 32 --private True --epsilon 3 --model bart
+python training.py --dataset phishing --epochs 8 --batch_size 32 --private True --epsilon 3 --model bart
+python training.py --dataset swmh --epochs 8 --batch_size 32 --private True --epsilon 3 --model bart
+python training.py --dataset thumbs-up --epochs 8 --batch_size 32 --private True --epsilon 3 --model bart
+python training.py --dataset webmd --epochs 8 --batch_size 32 --private True --epsilon 3 --model bart
 
 # e8
 python training.py --dataset phishing --epochs 8 --batch_size 32 --private True --epsilon 8 --model bart
