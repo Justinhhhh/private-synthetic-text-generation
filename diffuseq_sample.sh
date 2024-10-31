b6c7b19324976bdd697e3459d898ffe8c552fcbb
SebOchs <48994875+SebOchs@users.noreply.github.com>
2024-10-31 12:01:32 +0100

    Update diffuseq_sample.sh

--- diffuseq_sample.sh
+++ diffuseq_sample.sh
@@ -1,14 +1,3 @@
-#!/bin/bash
-#
-#SBATCH --job-name=dif-sample
-#SBATCH --output=/ukp-storage-1/ochs/thumbs-up-np-dif-sample.txt
-#SBATCH --account=ukp-researcher
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=1
-#SBATCH --mem=128GB
-#SBATCH --gres=gpu:1
-
-
 cd DiffuSeq
 
 # phishing
@@ -17,8 +6,8 @@ python decode.py --model_path ../models/phishing/eps8/400000.pt
 python decode.py --model_path ../models/phishing/np/200000.pt
 
 # swmh
-# python decode.py --model_path ../models/swmh/eps3/400000.pt
-# python decode.py --model_path ../models/swmh/eps8/400000.pt
+python decode.py --model_path ../models/swmh/eps3/400000.pt
+python decode.py --model_path ../models/swmh/eps8/400000.pt
 python decode.py --model_path ../models/swmh/np/200000.pt
 
 # thumbs-up
