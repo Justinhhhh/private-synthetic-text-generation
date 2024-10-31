d1f0a98a6daaa20242ec4bebcf609d96200710f6
SebOchs <48994875+SebOchs@users.noreply.github.com>
2024-10-31 12:03:23 +0100

    Update phi_train.sh

--- phi_train.sh
+++ phi_train.sh
@@ -1,13 +1,3 @@
-#!/bin/bash
-#
-#SBATCH --job-name=phi-baseline
-#SBATCH --output=/ukp-storage-1/ochs/phi-baseline.txt
-#SBATCH --account=ukp-researcher
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=1
-#SBATCH --mem=64GB
-#SBATCH --gres=gpu:a100:1
-
 cd baselines
 
 # np
